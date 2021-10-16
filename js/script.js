db26759babb1d7f7444d7becdf2d6db4a3ad8fb7
charenming <ysem88@gmail.com>
2021-10-16 22:48:53 +0900

    javaPteam

--- js/script.js
+++ js/script.js
@@ -45,20 +45,60 @@ $(function () {
 $(function () {
   //밑에 점 버튼
   document.querySelector(".button1").addEventListener("click", function () {
-    document.querySelector("#image_wapper_container").style.transform =
+    document.querySelector(".image_wapper_container").style.transform =
       "translate(0px)";
   });
   document.querySelector(".button2").addEventListener("click", function () {
-    document.querySelector("#image_wapper_container").style.transform =
+    document.querySelector(".image_wapper_container").style.transform =
       "translate(-1120px)";
   });
+
+  //밑 점버튼 (모바일)
+  document.querySelector(".mbutton1").addEventListener("click", function () {
+    document.querySelector("#image_mobile_container").style.transform =
+      "translate(0px)";
+  });
+  document.querySelector(".mbutton2").addEventListener("click", function () {
+    document.querySelector("#image_mobile_container").style.transform =
+      "translate(-570px)";
+  });
+  document.querySelector(".mbutton3").addEventListener("click", function () {
+    document.querySelector("#image_mobile_container").style.transform =
+      "translate(-1140px)";
+  });
+  document.querySelector(".mbutton4").addEventListener("click", function () {
+    document.querySelector("#image_mobile_container").style.transform =
+      "translate(-1710px)";
+  });
+  document.querySelector(".mbutton5").addEventListener("click", function () {
+    document.querySelector("#image_mobile_container").style.transform =
+      "translate(-2280px)";
+  });
+  document.querySelector(".mbutton6").addEventListener("click", function () {
+    document.querySelector("#image_mobile_container").style.transform =
+      "translate(-2850px)";
+  });
+
   //화살표 버튼
   document.querySelector(".button_prev").addEventListener("click", function () {
-    document.querySelector("#image_wapper_container").style.transform =
+    document.querySelector(".image_wapper_container").style.transform =
       "translate(0px)";
   });
   document.querySelector(".button_next").addEventListener("click", function () {
-    document.querySelector("#image_wapper_container").style.transform =
+    document.querySelector(".image_wapper_container").style.transform =
       "translate(-1120px)";
   });
 });
+//반응형
+/*window.onresize = function(event){
+var innerWidth = window.innerWidth;
+if(innerWidth <= 570){
+var script1 =
+'<div id="service_name1", class="service_name"><br><h3>부가 서비스</h3></div>'+
+'<img src="../images/공간/service1.png">'+
+'<img src="../images/공간/service2.png">'+
+'<img src="../images/공간/service3.png">'+
+'<img src="../images/공간/service4.png">';
+}
+document.getElementById("service").innerHTML=script1;
+}*/
